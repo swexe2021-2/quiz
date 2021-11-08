5a77b3a092b0218a50ac1077490ef548caf18519
mk <1911140052d@ed.fuk.kindai.ac.jp>
2021-11-08 07:12:25 +0000

    quizcontroller

--- app/controllers/quizs_controller.rb
+++ app/controllers/quizs_controller.rb
@@ -1,2 +1,34 @@
 class QuizsController < ApplicationController
+  def index
+    @quizs = Question.all
+  end
+  
+  def new
+    @quiz = Question.new
+  end
+
+  def create
+    @quiz = Question.new(title: params[:quiz][:title],
+    question: params[:quiz][:question],
+    #img: params[:quiz][:img],
+    answer: params[:quiz][:answer],
+    comment: params[:quiz][:comment]
+    )
+    if @quiz.save
+      redirect_to '/'
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @quiz = Question.find(params[:id])
+  end
+  
+  def delete
+    quiz = Question.find(params[:id])
+    quiz.destroy
+    redirect_to '/'
+  end
+  
 end
