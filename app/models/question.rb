c708a090af051082a1d645bbe6ecab7d23c68175
Satomi Hoshino <1911140057p@ed.fuk.kindai.ac.jp>
2021-11-15 06:59:17 +0000

    バリデーション

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,7 @@
 class Question < ApplicationRecord
+    validates :title, presence: true
+    validates :question, presence: true
+    validates :answer, presence: true
+    validates :comment, presence: true
+    
 end
